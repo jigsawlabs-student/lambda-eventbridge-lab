f22049ba32e347eae7f9414ff6a17085b1ae4b9c
Jeff Katzy <jeffreyerickatz@gmail.com>
2023-05-13 23:43:02 -0400

    boto intros

---
+++ src/1_read_file.py
@@ -0,0 +1,12 @@
+import boto3
+s3 = boto3.client('s3')
+
+# s3.list_buckets()
+
+# s3.list_objects(Bucket='jigsaw-initial-bucket')
+
+# obj = s3.get_object(Bucket='jigsaw-initial-bucket', Key='yelp-lunch-nyc.csv')
+
+
+# bucket = s3client.create_bucket()
+
